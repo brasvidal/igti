a2753e26afc2172730805410f86652f084dffab6
Marcos Vidal <marcosvidal@outlook.com>
2020-05-16 11:55:09 -0300

    Atualização

--- desafio1/script/slider.js
+++ desafio1/script/slider.js
@@ -1,7 +1,10 @@
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = this.value;
+var slider = document.getElementsByClassName('Slider');
+var txtred = document.getElementById('txtred');
+var txtgreen = document.getElementById('txtgreen');
+var txtblue = document.getElementById('txtblue');
+if (slider. {
+  txtred.value = document.getElementById('slider-red').value;
+  txtgreen.value = document.getElementById('slider-green').value;
+  txtblue.value = document.getElementById('slider-blue').value;
+}
+console.log(txtred.value);
