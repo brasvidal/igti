fdc831d82477984a20f6edf9234721f11151d4d7
Marcos Vidal <marcosvidal@oulook.com>
2020-05-21 18:14:45 -0300

    fetch

--- Aula-11-manipulacao-arrays/js/script.js
+++ Aula-11-manipulacao-arrays/js/script.js
@@ -1,12 +1,18 @@
-//Declaração de função anonima de carregamento da pagina
-window.addEventListener('load', () => {
-  const nomeEmailArray = results.map((person) => {
-    return {
-      name: person.name,
-      email: person.email,
-    };
-  });
+const dados = 'https://randomuser.me/api/?results=100';
+fetch(dados, {
+  method: 'get',
+}).then(function (response) {
+  response.json().then(function (data) {
+    function doMap() {
+      const nameEmailArray = data.results.map((person) => {
+        return {
+          name: person.name,
+          email: person.email,
+        };
+      });
+      return nameEmailArray;
+    }
 
-  console.log(nomeEmailArray);
-  //todo
+    console.log(doMap());
+  });
 });
